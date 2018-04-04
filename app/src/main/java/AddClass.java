9a04ad96918a8defd88ed30282ce8deba87a4f27
naito midori <naito.le@arkray.co.jp>
2018-04-04 10:59:09 +0900

    新規クラスの追加

--- app/src/main/java/AddClass.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by 7975 on 2018/04/04.
- */
-
-public class AddClass {
-
-    //コミットで　GitHubに追加されるか確認します。
-}
---
+++ app/src/test/java/com/example/a7975/git_test/AddClass.java
@@ -0,0 +1,7 @@
+package com.example.a7975.git_test;
+
+public class AddClass {
+
+    //コミットで　GitHubに追加されるか確認します。
+
+}
